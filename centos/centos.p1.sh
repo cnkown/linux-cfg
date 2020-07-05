9c29957c56177b32e435c90d6bf285641e20b933
cnkown <2419607665@qq.com>
2020-07-05 17:49:33 +0800

    change little with systemctl enable firewalld

--- centos/centos.p1.sh
+++ centos/centos.p1.sh
@@ -54,6 +54,7 @@ environment_Install() {
 
 firewall_on() {
     systemctl start firewalld
+    systemctl enable firewalld
     systemctl status firewalld
     firewall-cmd --zone=public --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp --add-port=2443/tcp --add-port=9091/tcp --add-port=26929/tcp --permanent
     firewall-cmd --reload 
