01018b215f7c0ba2afa05a27a1313387228c0cab
zengke <ke>
2020-12-06 10:07:34 +0800

    实例安装

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -34,15 +34,13 @@ reboot_os() {
 }
 
 environment_Install() {
-    apt -y install epel-release
     apt -y install net-tools wget curl firewalld
-    apt -y install java gcc python3 python3-pip
+    apt -y install gcc python3 python3-pip
     apt -y install screen tar
     apt -y install vim
-    apt -y install lsmod lsof
     cd ..
     mv vimrc /etc/
-    cd centos
+    cd $START_PATH
     apt -y update #更新全部安装，is same as apt upgarde
     pip3 install --upgrade pip
     echo -e "${Msg_Info}生产环境安装完成！\\n"
@@ -63,8 +61,7 @@ hardware_Check() {
 }
 
 system_Status() {
-    curl -o /etc/apt.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo && \
-    apt -y install neofetch redhat-lsb-core
+    apt -y install neofetch 
 }
 
 mermory_check() {
@@ -75,28 +72,8 @@ mermory_check() {
 }
 
 net_Check() {
-    pip3 install speedtest_cli
-    echo 0
-    mkdir Speedtest_Shell && cd Speedtest_Shell && \
+    pip3 -y install speedtest_cli
     wget https://ilemonra.in/LemonBenchIntl && mv LemonBenchIntl LemonBenchIntl.sh && chmod u+x LemonBenchIntl.sh && \
-    cd ${START_PATH}
-}
-
-kernel_Update() {
-    apt install -y https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm && \  #导入epel-repo源 centos7
-    echo 0                                                                                 #     apt install -y https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm &&\
-    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org && \                        #导入密钥
-    apt clean all && rm -rf /var/cache/apt && \                                            #清楚yum缓存
-    echo 0                                                                                 #     apt --disablerepo="*" --enablerepo="elrepo-kernel" list available | grep kernel-ml &&\  #检查当前能安装的内核版本
-    apt -y install --enablerepo=elrepo-kernel kernel-ml && \                               #安装最新内核
-    echo '将GRUB_TIMEOUT=5 改为 1 即等待 1 秒后启动'
-    vim /etc/default/grub && \                   #将GRUB_TIMEOUT=5 改为 1 即等待 1 秒后启动
-    grub2-mkconfig -o /boot/grub2/grub.cfg && \  #重新生成启动菜单列表
-    echo '#确认启动顺序，index=0 的内核版本应该等于刚刚更新的内核版本即为正确'
-    grubby --info=ALL && \  #确认启动顺序，index=0 的内核版本应该等于刚刚更新的内核版本即为正确
-    read -p "enter the serial number of the recently installed kernel. [0/1/2...] " num && \
-    grub2-set-default $num && \
-    reboot_os
 }
 
 main() {
